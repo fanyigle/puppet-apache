ddb31d9f381d5e75ff467325736b262bf6935086
root <root@stepehenan1c.mylabserver.com>
2020-04-04 02:31:53 +0000

    Added new params.pp to inherit vars

---
+++ manifests/params.pp
@@ -0,0 +1,14 @@
+# @summary
+#   Use Class inheritance to evaluate and passover variables,specially OS family
+class apache::params {
+  $install_ensure = 'present'
+  case $::osfamily {
+    'RedHat': {
+      $install_name = 'httpd'
+    }
+    'Debian': {
+      $install_name = 'apache2'
+    }
+      
+  }
+}
